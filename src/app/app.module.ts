dc5aeddb2db7017e11927d5a0e4b8b68209c8757
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-02-27 23:11:18 -0300

    Carregando produtos de uma dada categoria

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { ErrorInterceptorProvider } from '../interceptors/error-interceptor';
 import { StorageServices } from '../services/storage.service';
 import { ClienteService } from '../services/domain/cliente.service';
 import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
+import { ProdutoService } from '../services/domain/produto.service';
 
 @NgModule({
   declarations: [
@@ -36,7 +37,8 @@ import { AuthInterceptorProvider } from '../interceptors/auth-interceptor';
     ErrorInterceptorProvider,
     AuthService,
     StorageServices,
-    ClienteService
+    ClienteService,
+    ProdutoService,
   ]
 })
 export class AppModule {}
