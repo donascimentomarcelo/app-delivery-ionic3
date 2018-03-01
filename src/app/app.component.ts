189ebacb27cc796596d3e262c32649193f399523
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-02-28 23:55:55 -0300

    Criando tela de escolha de endereço

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -26,7 +26,6 @@ export class MyApp {
     this.pages = [
       { title: 'Profile', component: 'ProfilePage' },
       { title: 'Categorias', component: 'CategoriasPage' },
-      { title: 'Carrinho', component: 'CartPage' },
       { title: 'Logout', component: '' },
     ];
 
