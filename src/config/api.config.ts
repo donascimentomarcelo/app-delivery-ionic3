628e95ea25c86d7ef105596fa44d467ea80498af
Marcelo Nascimento <marcelojunin2010@hotmail.com>
2018-03-03 14:23:35 -0300

    Pegando imagem pela galeria

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
-   // baseUrl: "http://localhost:8080",
-    baseUrl: "https://modelorelacional.herokuapp.com/",
+    baseUrl: "http://localhost:8080",
+  //  baseUrl: "https://modelorelacional.herokuapp.com/",
 
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/modelo-conceitual"
 }
